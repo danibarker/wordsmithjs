f38d37d6e3b1b85e80bc6eff2670dd80bf926188
Danielle Barker <caseybarker86@gmail.com>
2021-12-19 21:25:52 -0700

    fixed url encoding

--- src/model/dictionary.js
+++ src/model/dictionary.js
@@ -1,47 +1,68 @@
 import axios from 'axios'
 let host = 'http://dictionary-env.eba-vuan7pqx.us-east-1.elasticbeanstalk.com/'
 export const define = async (stem, ...options) => {
+  stem = encodeURI(stem)
   const answer = await axios.get(host+`define/${stem}`)
   return answer.data
 };
 export const related = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`related/${stem}`)
   return answer.data
 };
 export const beginsWith = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`begins/${stem}`)
   return answer.data
 };
 export const contains = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`contains/${stem}`)
   return answer.data
 };
 export const hidden = async (stem, ...options) => {};
 export const pattern = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`pattern/${stem}`)
   return answer.data
 };
 export const regex = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`regex/${stem}`)
   return answer.data
 };
 export const endsWith = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`ends/${stem}`)
   return answer.data
 };
 export const check = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`check/${stem}`)
   return answer.data
 };
 export const hook = async (stem, ...options) => {
+  stem = encodeURI(stem)
+
   const answer = await axios.get(host+`hook/${stem}`)
   return answer.data
 };
 export const randomWord = async (wordLength, ...options) => {
+ 
+
   const answer = await axios.get(host+`random/${stem}`)
   return answer.data
 };
 export const anagram = async (rack, ...options) => {
+  rack = encodeURI(rack)
+
   const answer = await axios.get(host+`anagram/${rack}`)
   return answer.data
 };
